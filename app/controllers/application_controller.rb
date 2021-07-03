8dc8356f652f883bdacee612414906799a0185d2
kataiga <schone.alexandre@gmail.com>
2021-07-03 04:01:45 +0200

    API MGL

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::API
+    #quanf l'erreur RecordNotDestroyed et renvoyer peut importe ou dans l'application on utilise la methode not_destroyed
+    rescue_from ActiveRecord::RecordNotDestroyed, with: :not_destroyed
+
+    private
+
+    def not_destroyed(e)
+        render json: {errors: e.record.errors }, status: :unprocessable_entity
+    end
 end
