4ddda270f5393ffb64a1ed8b2ee2bb2e6829ba52
kataiga <77661981+kataiga@users.noreply.github.com>
2021-07-05 17:13:26 +0200

    Add files via upload

--- app/models/game.rb
+++ app/models/game.rb
@@ -3,4 +3,10 @@ class Game < ApplicationRecord
     validates :game_name, presence: true, length: { minimum: 1 }
     validates :description, presence: true, length: {minimum: 10 }
     validates :release, presence: true
+
+    belongs_to :editor
+    belongs_to :devloper
+    has_and_belongs_to_many :platforms
+    has_and_belongs_to_many :themes
+    has_and_belongs_to_many :genres
 end
